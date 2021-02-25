f5abbaac2bb16a689ca3a0af5d562ecccf268849
Dieghonm <dieghonm@gmail.com>
2021-02-25 19:57:15 -0300

    exercicios 4.2 ate o 6 em andamento

---
+++ exercices/bloco_4/4.2/4.2a.js
@@ -0,0 +1,10 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+for (let cont = 0; cont < groceryList.length; cont++){
+  console.log(groceryList[cont])
+}
---
+++ exercices/bloco_4/4.2/4.2b.js
@@ -0,0 +1,45 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for(let cont = 0; cont < numbers.length; cont++){
+// console.log(numbers[cont])
+// }
+
+let soma = 0
+
+// for(let cont = 0; cont < numbers.length; cont++){
+//   soma = soma + numbers[cont]
+// }
+// console.log(soma)
+
+// soma=0
+// for(let cont = 0; cont < numbers.length; cont++){
+//   soma = soma + numbers[cont]
+// }
+// let media = (soma/numbers.length)
+// console.log(media)
+// if(media>20){
+//   console.log("valor maior que 20")
+// }else("valor menor ou igual a 20")
+let maior = 0
+// for(let cont = 0; cont< numbers.length; cont++){
+//   if (maior<numbers[cont]){
+//     maior = numbers[cont]
+//   }
+// }
+// console.log(maior)
+
+
+exercicio 6 em andamento
+let impar = 0
+for(let cont = 0; cont< numbers.length; cont++){
+  let numero=numbers[cont]
+  if (numero % 2 = 1){
+    impar++
+  }else{
+    console.log("nenhum valor ímpar encontrado")
+  }
+}
+console.log(impar)
+
+
+
